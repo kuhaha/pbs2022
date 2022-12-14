34a12edbbfa7224a77506a63633697457c336a0f
kuhaha <kevin.yr.cheng@gmail.com>
2022-12-14 16:55:54 +0900

    Refactor

--- src/ad_reply_save.php
+++ src/ad_reply_save.php
@@ -2,7 +2,7 @@
 require_once('db_inc.php');
 $id = $_POST['id'];
 $ad = $_POST['advice'] ;
-$sql="UPDATE tbl_progress SET advice='{$ad}',`read`=now() WHERE id=$id";
+$sql="UPDATE tbl_progress SET advice='{$ad}',read_date=now() WHERE id=$id";
 //echo $sql;
 $rs = $conn->query($sql);
 if (!$rs) die('エラー: ' . $conn->error);
